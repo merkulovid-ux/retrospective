8de9a23c95969b72d6a6791ff42ea069bd70cfed
unknown <U_M16X2@HNBA131636.alfaextra.net>
2025-09-17 12:56:35 +0300

    feat(sprint-11): start sprint and fix e2e test pipeline

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,6 +2,11 @@ import { defineConfig } from "@playwright/test";
 
 export default defineConfig({
   testDir: "tests",
+  webServer: {
+    command: "npx http-server -p 8080 -c-1 .",
+    url: "http://localhost:8080/",
+    reuseExistingServer: !process.env.CI,
+  },
   use: {
     baseURL: process.env.BASE_URL || "http://localhost:8080/",
     trace: "on-first-retry",
